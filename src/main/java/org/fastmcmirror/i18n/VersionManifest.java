80e672dfea42334c2d74fc4ef91ff5c2c06ebed0
killerprojecte <admin@fastmcmirror.org>
2024-12-07 19:17:28 +0800

    Optimize

---
+++ src/main/java/org/fastmcmirror/i18n/VersionManifest.java
@@ -0,0 +1,12 @@
+package org.fastmcmirror.i18n;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Version {
+    @SerializedName("versions")
+    private List<Version> versions;
+
+    public List<Version> getVersions() {
+        return id;
+    }
+}
--- src/main/java/org/fastmcmirror/i18n/VersionUtil.java
+++ src/main/java/org/fastmcmirror/i18n/VersionUtil.java
@@ -13,10 +13,9 @@ public class VersionUtil {
         long starttime = System.currentTimeMillis();
         String versionjson = Util.FileToString(Util.dlFile("https://bmclapi2.bangbang93.com/mc/game/version_manifest.json", "https://launchermeta.mojang.com/mc/game/version_manifest.json", savePath, "versions.json"));
         JsonObject vjo = new JsonParser().parse(versionjson).getAsJsonObject();
-        JsonArray vja = vjo.get("versions").getAsJsonArray();
-        for (JsonElement je : vja) {
-            JsonObject object = je.getAsJsonObject();
-            versions.put(object.get("id").getAsString(), new Gson().fromJson(object, Version.class));
+        VersionManifest manifest = new Gson().fromJson(vjo, VersionManifest.class);
+        for (Version version : manifest.getVersions()) {
+            version.put(version.getId(), version);
         }
         //System.out.println("Getting the version list took " + (System.currentTimeMillis() - starttime) + "ms");
     }
